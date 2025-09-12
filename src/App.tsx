a70986ed267a8fa4bbe89dedafbfa409ec6c5e6f
Yazi0 <yasiru01nimsara@gmail.com>
2025-09-12 11:13:26 +0530

    Second Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import HomePage from "./pages/Home";
 import ProgramsPage from "./pages/ProgramsPage";
 import ApplyPage from "./pages/ApplyPage";
 import NewsDetailPage from "./pages/NewsDetailPage";
 import NewsPage from "./pages/NewsPage";
 import FacultyPage from "./pages/FacultyPage";
-import StudentLifePage from "./pages/StudentLifePage";
 import EventsPage from "./pages/EventsPage";
-import CareersPage from "./pages/CareersPage";
 import FAQPage from "./pages/FAQPage";
 import GalleryPage from "./pages/GalleryPage";
 import EventDetailsPage from "./pages/EventDetailsPage";
@@ -16,20 +13,21 @@ import Contact from "./pages/contact";
 import AboutPage from "./pages/AboutPage";
 import DownloadsPage from "./pages/DownloadsPage";
 import BlogPage from "./pages/BlogPage";
+import { Home } from "lucide-react";
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/programs" element={<ProgramsPage />} />
         <Route path="/apply" element={<ApplyPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/news" element={<NewsPage />} />
         <Route path="/news/:id" element={<NewsDetailPage />} />
         <Route path="/faculty" element={<FacultyPage />} />
-        <Route path="/student-life" element={<StudentLifePage />} />
-        <Route path="/careers" element={<CareersPage />} />
         <Route path="/events" element={<EventsPage />} />
         <Route path="/faq" element={<FAQPage />} />
         <Route path="/gallery" element={<GalleryPage />} />
@@ -37,6 +35,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/downloads" element={<DownloadsPage />} />
          <Route path="/blog" element={<BlogPage />} /> 
+        
       </Routes>
     </Router>
   );
